43475cc4ad10582a7004e81f5e69f63469e72318
Rindra R <rindranirina@gmail.com>
2016-06-11 16:00:52 -0700

    added onEnter handler

--- src/Components/Card.elm
+++ src/Components/Card.elm
@@ -4,24 +4,31 @@ import Date exposing (Date)
 import Html exposing (..)
 import Html.App as Html
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onInput, keyCode, targetValue, on)
+import Json.Decode as Json
+import Dict exposing (Dict)
 
 type alias Task = 
   { name : String
   }
-type alias Model = List Task
+type alias Model = { tasks : Dict Int Task, nextId : Int}
 
 
 model : Model
-model = []
+model = { tasks = Dict.empty, nextId = 0 }
 
 type Msg 
   = AddItem String
+  | NoOp
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of 
-    AddItem task -> { name = task } :: model
+  case (msg, model) of 
+    (AddItem taskName, { tasks, nextId }) -> 
+      let 
+        newTasks = Dict.insert nextId { name = taskName } tasks
+      in { tasks = newTasks, nextId = nextId + 1 }
+    (NoOp, model) -> model
 
 view : Model -> Html Msg
 view model = 
@@ -29,42 +36,63 @@ view model =
     cardStyle = 
       style <| List.concatMap identity
                 [ backColor, size, alignment, misc ]
+
+    itemView : Task -> Html Msg
     itemView = 
       \entity -> 
-        input 
-          [
-            type' "text"
-          , class "form-control input-sm col-xs-1"
-          , value entity.name
-          , style [
-                ("margin-top", "10px")
-              , ("margin-bottom", "5px")
-            ]
-          ] []
+        let
+          place = ""
+        in 
+          input 
+            [
+              type' "text"
+            , class "form-control input-sm col-xs-1"
+            , placeholder "Enter task name"
+            , value entity.name
+            , style [
+                  ("margin-top", "10px")
+                , ("margin-bottom", "5px")
+              ]
+            ] []
 
-    newItem = 
+    newItemInput = 
       input 
         [
           type' "text"
         , class "form-control input-sm col-xs-1"
         , placeholder "New Task"
-        , onInput AddItem
+        , onEnter NoOp AddItem
         , style [
               ("margin-top", "10px")
-            , ("margin-bottom", "5px")
+            , ("margin-bottom", "10px")
           ]
         ] []
 
-    items = newItem :: (List.map itemView model)
+    taskList = Dict.values model.tasks
 
-  in div [cardStyle, class "col-md-3"] items
+    items = newItemInput :: (List.map itemView taskList)
+
+  in div [] 
+      [ div [cardStyle, class "col-md-3"] (items)
+      , text << toString <| model
+      ]
+
+-- EventListener function
+onEnter fail success =
+  let
+    tagger : Int -> String -> msg
+    tagger code val =
+      if code == 13 then success val
+      else fail
+  in
+    on "keyup" (Json.object2 tagger keyCode targetValue)
+
 
 -- CSS styles
 backColor = [ ("background-color", "lightblue") ]
 noBorder = [ ("border", "none") ]
 size = 
   [ ("width" , "150px")
-  , ("height" , "150px")
   ]
 position = 
   [ 
@@ -78,6 +106,7 @@ alignment =
   , ("margin-right", "5px")
   , ("margin-top", "5px")
   , ("margin-bottom", "5px")
+  , ("padding", "10px")
   ]
 misc = 
   [ ("border-radius" , "10px")
